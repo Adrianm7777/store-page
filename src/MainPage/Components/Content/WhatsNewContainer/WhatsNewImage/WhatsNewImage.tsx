a9d085960a20ab2f0f0d9837993309cd2fdc6fe4
Adrianm7777 <Adrianmaterka10@gmail.com>
2023-11-20 13:47:57 +0100

    bug fixed

--- src/MainPage/Components/Content/WhatsNewContainer/WhatsNewImage/WhatsNewImage.tsx
+++ src/MainPage/Components/Content/WhatsNewContainer/WhatsNewImage/WhatsNewImage.tsx
@@ -3,7 +3,7 @@ import styles from "./WhatsNewImage.module.scss";
 import WhatsNewPanels from "./WhatsNewPanels.consts";
 
 const WhatsNewImage = () => (
-  <Container fluid className="d-flex flex-wrap h-80 mt-3">
+  <Container fluid className="d-flex flex-wrap h-75 mt-3">
     {WhatsNewPanels.map(({ href, imgSrc }) => (
       <a href={href} className={`m-3 ${styles.whatsNewElement}`}>
         <div className={`position-relative ${styles.elementWrapper}`}>
